d6aee29b83bd8f08402f3750cf81c1189e57575e
lillefrog <Christian_Brandt@yahoo.com>
2014-05-27 17:04:59 +0100

    Minor change to work file

--- BasicFile/TempWorkFile.m
+++ BasicFile/TempWorkFile.m
@@ -1,11 +1,15 @@
 
 
 
-function [NLXFile] = NLX_LoadAndCutCSCFile(NLXFileName,TimeStart,TimeStop);
+
 addpath(genpath('H:\GitHub\NeuralynxAnalysis\'));
 
+
+FName = 'C:\Dropbox\temp\TFT_2014-05-23_17-52-13\Events.Nev';
 [AutomaticEvents,ManualEvents] = NLX_ReadEventFile(FName);
 
 
+
+function [NLXFile] = NLX_LoadAndCutCSCFile(NLXFileName,TimeStart,TimeStop);
 
 function NLX_GetTimestampFromEvent(EventList,EventName);
