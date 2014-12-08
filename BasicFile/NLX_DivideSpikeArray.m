38ca02673694cd295dcadf6c5cd0d267dfb3f34c
lillefrog <Christian_Brandt@yahoo.com>
2014-12-08 17:32:41 +0000

    Added some pretrial spikes to NLX_DivideSpikeArray
    It now stores spikes 100ms before the start of the trial. There is no
    check to make sure that they do not belong to the previous trial so use
    with care.

--- BasicFile/NLX_DivideSpikeArray.m
+++ BasicFile/NLX_DivideSpikeArray.m
@@ -3,6 +3,8 @@ function [dividedSpikeArray] = NLX_DivideSpikeArray(spikeArray,dividedEventfile)
 
  nTrials = length(dividedEventfile);
  dividedSpikeArray = cell(1,nTrials); % initialize
+ 
+ preTrialTime = 100*1000; % save 100ms spikes just before the trial starts for baseline
 
  % get the start and stop times from each event trial and use them to
  % select any continous sampled data within that time frame. Any other data
@@ -10,7 +12,7 @@ function [dividedSpikeArray] = NLX_DivideSpikeArray(spikeArray,dividedEventfile)
  for i=1:nTrials
   startTime = min(dividedEventfile{i}(:,1));
   stopTime = max(dividedEventfile{i}(:,1));
-  withinTime = (spikeArray(:,1)>startTime) & (spikeArray(:,1)<stopTime);
+  withinTime = (spikeArray(:,1)>(startTime-preTrialTime)) & (spikeArray(:,1)<stopTime);
   dividedSpikeArray{i} = spikeArray( withinTime,:);
  end
 
